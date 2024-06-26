a8059e8fbe62f89de4687510ebda58742b22fce0
arafatGithubCode <md489288@gmail.com>
2024-06-26 10:24:43 +0600

    fix session url response of stripe

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -46,6 +46,8 @@ const createCheckoutSession = async (req: Request, res: Response) => {
     if (!session.url) {
       return res.status(500).json({ message: "Error creating stripe session" });
     }
+
+    res.json({ url: session.url });
   } catch (error: any) {
     console.error("Error", error);
     res.status(500).json({ message: error.raw.message });
